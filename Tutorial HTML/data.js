ab3379553ece21f1dd329a62b2fc131418de0065
Wetzxd <107447596+Wetzxd@users.noreply.github.com>
2024-07-09 19:16:40 +0700

    Add files via upload

---
+++ Tutorial HTML/data.js
@@ -0,0 +1,17 @@
+const namaSaya = "rifai mugni";
+let usia = 15;
+
+function generateBiodata() {
+    if ( usia > 10 && usia < 17) {
+// INI ADLH KONDISI PERTAMA
+        console.log('anda remaja');
+    } else {
+// KONDISI KETIKA TIDAK TERPENUHI
+        console.log('ajg')
+    }
+}
+
+console.log(namaSaya);
+console.log(usia);
+
+generateBiodata();
